681b991224825ba331d2bbcbd073f5b25768630e
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-05-27 20:48:24 +0100

    Tested sdl implementation

--- DSP/src/main.cpp
+++ DSP/src/main.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <SDL.h>
 
-int main()
+int main(int argc, char** agrs)
 {
+    SDL_Init(SDL_INIT_AUDIO);
+
     printf("Lets start with DSP");
+
+    SDL_Quit();
     return 0;
 }
