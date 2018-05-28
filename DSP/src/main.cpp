51dae3129ad10a7bc11a552b054d16905948870c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-05-28 20:14:15 +0100

    Actually playing sound and pausing it
    Stop still doesn't work
    Program crashes at end of audio

--- DSP/src/main.cpp
+++ DSP/src/main.cpp
@@ -1,11 +1,49 @@
-#include <stdio.h>
+#include <iostream>
 #include <SDL.h>
+
+#include "sdl/SDLAudioDevice.h"
+#include "sdl/SDLAudioContext.h"
+
+#define FILE_PATH "res/gong-16-bit.wav"
 
 int main(int argc, char** agrs)
 {
     SDL_Init(SDL_INIT_AUDIO);
 
-    printf("Lets start with DSP");
+    IAudioDevice*  device  = new SDLAudioDevice();
+    IAudioContext* context = new SDLAudioContext();
+
+    
+    IAudioData* data = device->CreateAudioFromFile(FILE_PATH);
+    
+    SampleInfo info;
+    info.volume = 1.0;
+
+    AudioObject sound(info, data);
+
+    char in = 0;
+    while (in != 'q')
+    {
+        std::cin >> in;
+        switch (in) 
+        {
+        case 'a':
+            context->PlayAudio(sound);
+            break;
+        case 's':
+            context->PauseAudio(sound);
+            break;
+        case 'd':
+            context->StopAudio(sound);
+            break;
+        default:
+            break;
+        }
+    }
+
+    device->ReleaseAudio(data);
+    delete context;
+    delete device;
 
     SDL_Quit();
     return 0;
