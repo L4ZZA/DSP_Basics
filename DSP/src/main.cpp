f8eaea6cddea33348ef84644f0bbd30056ed6345
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-06-05 00:12:06 +0100

    Testing SFML audio

--- DSP/src/main.cpp
+++ DSP/src/main.cpp
@@ -1,9 +1,27 @@
+#include <SFML/Audio.hpp>
+
 #include "base/easy_input.h"
 
 #define ESC 27
 
-int main()
+int main(void)
 {
-    while (int input = getChar() != ESC);
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("res/gong-16-bit.wav"))
+        return -1;
+    
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+
+    char input(0);
+    while (input != ESC)
+    {
+        input = getChar();
+        if (input == 'p')
+        {
+            sound.play();
+        }
+    }
+
     return 0;
 }
